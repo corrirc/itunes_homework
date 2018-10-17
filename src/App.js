94f8856d791638a0b2aaebc4fabc0e1cd367b5f5
Ricky <rickycorrigan@icloud.com>
2018-10-17 07:48:30 +0100

    adds MusicContainer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import MusicContainer from './containers/MusicConitainer.js';
 
 
 class App extends Component {
   render() {
     return (
-
+      <MusicContainer />
     );
   }
 }
